41e4768028f2abe5413049876722dc1c3addab85
Kristina Camia <kcamia19@gmail.com>
2019-11-05 13:18:42 -0500

    grades fixed

--- src/grades.js
+++ src/grades.js
@@ -5,24 +5,31 @@ let gradeInput = Number(readlineSync.question("\nEnter a grade: "));
 const MIN = 0
 const MAX = 100
 
-const aRange = 90 >= gradeInput <= 100
-const bRange = 80 >= gradeInput <= 89
-const cRange = 70 >= gradeInput <= 79
-const dRange = 60 >= gradeInput <= 69
-const fRange = 0 >= gradeInput <= 59
+const A_MAX = 100
+const A_MIN = 90
+const B_MAX= 89
+const B_MIN = 80
+const C_MAX = 79
+const C_MIN = 70
+const D_MAX = 69
+const D_MIN = 60
+const F_MAX = 59
+const F_MIN = 0
 
 if (Number.isNaN(gradeInput)) {
   console.log("\nInvalid.")
-} else if (gradeInput < MIN|| gradeInput > MAX) {
+} else if (gradeInput < MIN || gradeInput > MAX) {
   console.log("\nInvalid.")
-} else if (gradeInput = aRange) {
+} else if (A_MAX >= gradeInput && gradeInput >= A_MIN) {
   console.log("\nYou received an A.")
-} else if (gradeInput = bRange) {
+} else if (B_MAX >= gradeInput && gradeInput >= B_MIN) {
   console.log("\nYou received a B.")
-} else if (gradeInput = cRange) {
+} else if (C_MAX >= gradeInput && gradeInput >= C_MIN) {
   console.log("\nYou received a C.")
-} else if (gradeInput = dRange) {
+} else if (D_MAX >= gradeInput && gradeInput >= D_MIN) {
   console.log("\nYou received a D.")
-} else if (gradeInput = fRange) {
+} else if (F_MAX >= gradeInput && gradeInput >= F_MIN) {
   console.log("\nYou received an F.")
+} else {
+  console.log("\nInvalid.")
 }
